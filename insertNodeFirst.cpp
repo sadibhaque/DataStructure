a41edbe4e6f35089fcb477ce1f31be27733272ca
sadibhaque <sadibhaquee@gmail.com>
2023-09-04 03:06:40 +0600

    1

--- insertNodeFirst.cpp
+++ insertNodeFirst.cpp
@@ -1,7 +1,46 @@
-#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
 
-using namespace std;
+struct node{
+    int data;
+    struct node *next;
+};
 
-int main() {
-    //sdasd
+typedef struct node node;
+
+node *head = NULL;
+
+void insertFirst(int x){
+    node *current = (node*)malloc(sizeof(node));
+    current -> data = x;
+    current -> next = head;
+    head = current; 
+}
+
+void printList(){
+    node *current = head;
+
+    while(current != NULL){
+        printf("%d  ",current->data);
+        current = current -> next;
+    }
+}
+
+int main(){
+    node *node1 = (node*)malloc(sizeof(node));
+    node1->data = 5;
+    node1->next = NULL;
+    head = node1;
+
+    int n,x;
+
+    printf("Number of Node : ");
+    scanf("%d",&n);
+
+    for(int i=0; i<n; i++){
+        printf("Data for Current Node : ");
+        scanf("%d",&x);
+        insertFirst(x);
+    }
+    printList();
 }
--- insertNodeLast.cpp
+++ insertNodeLast.cpp
@@ -1,18 +1,58 @@
-#include <bits/stdc++.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-using namespace std;
+struct node{
+    int data;
+    struct node *next;
+};
+
+typedef struct node node;
+
+node *head = NULL;
 
 
+void insertLast(int x) {
+    node *newNode = (node*)malloc(sizeof(node));
+    newNode->data = x;
+    newNode->next = NULL;
 
-int main() {
-    int n, x;
-
-    cin>>n;
-
-    for (int i = 0; i < n; i++) {
-        printf("dffdf");
+    if (head == NULL) {
+        head = newNode;
+        return;
     }
-    printf("dfsdfsd");
+    node *current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newNode;
+}
 
-    return 0;
+
+void printList(){
+    node *current = head;
+    while(current != NULL){
+        printf("%d  ",current->data);
+        current = current -> next;
+    }
+}
+
+
+int main(){
+
+    node *node1 = (node*)malloc(sizeof(node));
+    node1->data = 5;
+    node1->next = NULL;
+    head = node1;
+
+    int n,x;
+
+    printf("Number of Node : ");
+    scanf("%d",&n);
+
+    for(int i=0; i<n; i++){
+        printf("Data for Current Node : ");
+        scanf("%d",&x);
+        insertLast(x);
+    }
+    printList();
 }
