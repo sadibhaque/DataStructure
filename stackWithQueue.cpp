a2476633700c54850d3b8cf0936e68ece19e7f55
Sadib <111662359+sadibhaque@users.noreply.github.com>
2023-09-13 22:43:18 +0600

    Create stackWithQueue.cpp

---
+++ stackWithQueue.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+ll que[100];
+ll f=0,r=0;
+
+void enq(int x){
+    que[r] = x;
+    r++;
+}
+void deq(){
+    f++;
+}
+int main(){
+    
+    enq(2);
+    enq(3);
+    enq(4);
+    deq();
+
+    for (int i = f; i < r; i++) cout<<que[i]<<" ";
+}
