692e9a2b4c8806a98fa650c29de731c18a1689ca
Sadib <111662359+sadibhaque@users.noreply.github.com>
2023-10-23 22:57:53 +0600

    1

--- graphWithAdjList.cpp
+++ graphWithAdjList.cpp
@@ -1,36 +1,37 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long int
 #define llf double
 
-void addEdge(vector<vector<ll>>& adj, ll x, ll y) {
-    adj[x].push_back(y); 
-    adj[y].push_back(x);
-}
+void solve() {
 
-void printAdjList(const vector<vector<ll>>& adj) {
-    for (ll i = 0; i < adj.size(); i++) {
+    ll vertices,edges;
+    cin >> vertices >> edges;
+
+    vector<ll> adjList[vertices];
+
+    for (int i = 0; i < edges; i++){
+        ll v1, v2;
+        cin >> v1 >> v2;
+
+        adjList[v1].push_back(v2);
+        adjList[v2].push_back(v1);
+    }
+
+    for (int i = 0; i < vertices; i++){
         cout << "Adjacency list for vertex " << i << ": ";
-        for (ll j = 0; j < adj[i].size(); j++) {
-            cout << adj[i][j] << " ";
+        for (int j = 0; j < adjList[i].size(); j++){
+            cout << adjList[i][j] << " ";
         }
         cout << endl;
     }
+
 }
 
-ll maxV = 5;
-
 int main() {
-    vector<vector<ll>> adj(maxV);
 
-    addEdge(adj, 0, 1);
-    addEdge(adj, 0, 2);
-    addEdge(adj, 0, 3);
-    addEdge(adj, 2, 1);
-    addEdge(adj, 2, 4);
-    addEdge(adj, 4, 4);
+    solve();
 
-    printAdjList(adj);
+    return 0;
 }
