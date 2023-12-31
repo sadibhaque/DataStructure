127983e594a5d4f9b41506d0a0feb450f11c7bb7
Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-01-01 00:27:35 +0600

    1

--- binaryTreeTraversal.cpp
+++ binaryTreeTraversal.cpp
@@ -1,34 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class node{
+#define ll long long int
+
+class Node{
 public:
     int data;
-    node* left;
-    node* right;
+    Node* left;
+    Node* right;
 
-    node(int d){
-        this-> data = d;
-        this -> left = NULL;
-        this -> right = NULL;
+    Node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-void preOrder(node* root){
+void levelTraverse(Node* root){
+    queue <Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        cout<<temp -> data<<" ";
+
+        if(temp -> left != NULL) q.push(temp -> left);
+        if(temp -> right != NULL) q.push(temp -> right);
+    }
+}
+
+void preOrder(Node* root){
     if(root == NULL) return;
     cout<<root->data<<" ";
     preOrder(root->left);
     preOrder(root->right);
 }
 
-void inOrder(node* root){
+void inOrder(Node* root){
     if(root == NULL) return;
     inOrder(root->left);
     cout<<root->data<<" ";
     inOrder(root->right);
 }
 
-void postOrder(node* root){
+void postOrder(Node* root){
     if(root == NULL) return;
     postOrder(root->left);
     postOrder(root->right);
@@ -38,17 +55,16 @@ void postOrder(node* root){
 int main(){
     freopen("input.txt", "r", stdin);
 
-    node* root = new node(10);
-    node* a = new node(20);
-    node* b = new node(30);
-    node* c = new node(40);
-    node* d = new node(50);
-    node* e = new node(60);
-    node* f = new node(70);
-    node* g = new node(80);
-    node* h = new node(90);
-    node* i = new node(100);
-
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+    Node* f = new Node(70);
+    Node* g = new Node(80);
+    Node* h = new Node(90);
+    Node* i = new Node(100);
 
     root -> left = a;
     root -> right = b;
@@ -60,7 +76,8 @@ int main(){
     d -> left = f;
     d -> right = g;
 
-
+    traverse(root);
+    cout<<endl;
     preOrder(root);
     cout<<endl;
     inOrder(root);
